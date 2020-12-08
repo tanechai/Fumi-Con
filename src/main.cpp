6660c83cff03c1db9f92427a44b28c31a1d1bf7c
tanechai <tiuseko0120@gmail.com>
2020-12-08 05:37:54 -0800

    warningを消し、動作するように変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,67 @@
 #include <iostream>
 #include <vector>
+
 #include <opencv2/opencv.hpp>
 
 
-/*
+class selectedData{
+public:
+    std::vector<cv::Point2d> selectedPoints;
+	int selectedNumber;
+    selectedData():selectedPoints(std::vector<cv::Point2d>(4)),selectedNumber(0){};
+};
+
 void mouse_callback(int event, int x, int y, int flags, void *userdata)
 {
-    if (event == cv::EVENT_LBUTTONDOWN && selectedPoints.size() < 4)
-    {
-        selectedPoints.push_back(cv::Point2f(x, y));
-
+    selectedData *p = (selectedData*)userdata;
+    if (event == cv::EVENT_LBUTTONDOWN) {
         std::cout << "Left mouse button is pressed." << std::endl;
-    }
-    if (event == cv::EVENT_MOUSEMOVE)
-    {
-        std::cout << "(" << x << ", " << y << ")" << std::endl;
-    }
-}*/
-
-int main()
-{
-    cv::VideoCapture cap(0); //デバイスのオープン
-
-    if (!cap.isOpened()) //カメラデバイスが正常にオープンしたか確認．
-    {
-        //読み込みに失敗したときの処理
-        return -1;
-    }
-    std::vector<cv::Point2f> selectedPoints;
-    cv::Mat frame;
-    cap.read(frame);
-    cv::imshow("example", frame);
-    //cv::setMouseCallback("example", mouse_callback);
-    while (selectedPoints.size() < 4)
-    {
-        
-        for (const cv::Point2f& p : selectedPoints)
-        {
-            cv::drawMarker(frame, p, cv::Scalar(0, 255, 0));
+        if(p->selectedNumber == 4){
+            std::cout << "All points selected" << std::endl;
+            return;
         }
-        std::cout << "(^^)" << std::endl;
-        cv::imshow("example2", frame);
+        p->selectedPoints.at(p->selectedNumber) = cv::Point2d(x,y);
+        p->selectedNumber++;
+        std::cout << x << "," << y << std::endl;
     }
-    cv::destroyWindow("example");
-
-    cv::waitKey();
-
-    return 0;
 }
-/*
-std::vector<cv::Point2f> selectPoints(int numberOfTime);
 
 int main(int argc, char** argv)
 {
+    cv::VideoCapture cap(0);
 
-    cv::Mat K = (cv::Mat_<double>(3,3) << 502.0371895909179, 0, 300.0349293832901, 0, 501.6613987605497, 240.5838189630333, 0, 0, 1);
-    cv::Mat distCoeffs = (cv::Mat_<double>(1,5) << -0.4093166455082794, 0.2602125340365962, 0.0005732983044178636, 3.803626762254057e-05, -0.1121563761094383);
-
-    std::vector<cv::Point2f> selectedPoints(4);
-    cv::TermCriteria criteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS,20, 0.001);
-
-    selectedPoints = selectPoints();
-
-    while(true){
-
-    }
-    std::vector<cv::Mat> imgs;
+    if(!cap.isOpened())
     {
-        util::camera cam(0);
-        std::cout << "caputure image" << std::endl;
-        while(1){
-            cam.show();
-            if(cv::waitKey(1)>0){
-                break;
-            }
-        }
-        cv::Mat img = cam.capture();
-        cv::Mat img_;
-        cv::undistort(img, img_, K, distCoeffs);
-        imgs.push_back(img_);
-        cv::cvtColor(imgs[0],img,cv::COLOR_RGB2GRAY);
-        checkerImgs.push_back(img);
-        std::cout << "Load image" << std::endl;
+        return -1;
     }
 
-    cv::Size2d imageSize(200.0,100.0);
-    cv::Mat transformedImg;
-    const std::vector<cv::Point2f> dst_pt{
-        cv::Point2f(0,0),
-        cv::Point2f(0,imageSize.height),
-        cv::Point2f(imageSize.width,0),
-        cv::Point2f(imageSize.width,imageSize.height)
-    };
-    
-    
+    cv::Mat frame;
+    cap.read(frame);
+    const std::string selectWinName = "select window";
+    selectedData data;
+    cv::namedWindow(selectWinName);
+    cv::imshow(selectWinName, frame);
+
+    cv::setMouseCallback(selectWinName, mouse_callback,&data);
     cv::waitKey();
-}
+    
+    if(data.selectedNumber < 4){
+        std::cout << "click more points" << std::endl;
+        return -1;
+    }
+    std::cout << "aaa" << std::endl;
+    while(true){
+        cap.read(frame);
+        for(int i = 0; i < 4; i++){
+            cv::circle(frame,data.selectedPoints.at(i),3,cv::Scalar(255,255,255));
+        }
+        
+        cv::imshow(selectWinName, frame);
+        if(cv::waitKey(10) > 0){
+            break;
+        }
+    }
+    cv::destroyAllWindows();
 
-std::vector<cv::Point2f> selectPoints(int numberOfTime){
-
+    return 0;
 }
-*/
