bf34f1b3ca98d575f7694eb0c011306640da4fc0
tanechai <tiuseko0120@gmail.com>
2020-12-09 14:56:51 +0900

    ホモグラフィ変換処理の追加と歪み切り取り範囲の拡張

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 
 class selectedData{
 public:
-    std::vector<cv::Point2d> selectedPoints;
+    std::vector<cv::Point2f> selectedPoints;
 	int selectedNumber;
-    selectedData():selectedPoints(std::vector<cv::Point2d>(4)),selectedNumber(0){};
+    selectedData():selectedPoints(std::vector<cv::Point2f>(4)),selectedNumber(0){};
 };
 
 void mouse_callback(int event, int x, int y, int flags, void *userdata)
@@ -34,14 +34,24 @@ int main(int argc, char** argv)
     {
         return -1;
     }
-
+    cv::Mat K = (cv::Mat_<double>(3,3) << 502.0371895909179, 0, 300.0349293832901, 0, 501.6613987605497, 240.5838189630333, 0, 0, 1);
+    cv::Mat distCoeffs = (cv::Mat_<double>(1,5) << -0.4093166455082794, 0.2602125340365962, 0.0005732983044178636, 3.803626762254057e-05, -0.1121563761094383);
+    
+    cv::Mat frame_;
     cv::Mat frame;
-    cap.read(frame);
+    cap.read(frame_);
+    
+    cv::Mat newK = cv::getOptimalNewCameraMatrix(K,distCoeffs,cv::Size2d(frame_.cols,frame_.rows),0.05);
+    cv::Mat R = cv::Mat::eye(3,3,CV_32F);
+    std::vector<cv::Mat> map(2);
+    cv::initUndistortRectifyMap(K,distCoeffs,R,newK,cv::Size2d(frame_.cols,frame_.rows),CV_32FC1,map.at(0),map.at(1));
+    cv::remap(frame_,frame,map.at(0),map.at(1),cv::INTER_AREA);
+    
+    //cv::undistort(frame_, frame, K, distCoeffs);
     const std::string selectWinName = "select window";
     selectedData data;
     cv::namedWindow(selectWinName);
     cv::imshow(selectWinName, frame);
-
     cv::setMouseCallback(selectWinName, mouse_callback,&data);
     cv::waitKey();
     
@@ -49,17 +59,34 @@ int main(int argc, char** argv)
         std::cout << "click more points" << std::endl;
         return -1;
     }
+    cv::Mat dst = cv::Mat::zeros(67*7, 67*10, CV_8UC3);
+    double para = 1;
+    double para2 = 30;
+    const std::vector<cv::Point2f> dst_pt={
+        cv::Point2f(dst.cols/2 - 3*para2/2,dst.rows/2 - 4*para2/2),
+        cv::Point2f(dst.cols/2 - 3*para2/2,dst.rows/2 + 4*para2/2),
+        cv::Point2f(dst.cols/2 + 3*para2/2,dst.rows/2 - 4*para2/2),
+        cv::Point2f(dst.cols/2 + 3*para2/2,dst.rows/2 + 4*para2/2)
+    };
+    
+    // homography 行列を計算
+    const cv::Mat homography_matrix = cv::getPerspectiveTransform(data.selectedPoints,dst_pt);
     while(true){
-        cap.read(frame);
+        cap.read(frame_);
+        cv::remap(frame_,frame,map.at(0),map.at(1),cv::INTER_AREA);
+        //cv::undistort(frame_, frame, K, distCoeffs);
         for(int i = 0; i < 4; i++){
             cv::circle(frame,data.selectedPoints.at(i),3,cv::Scalar(255,255,255));
         }
-        
+        // 透視変換
+        cv::warpPerspective( frame, dst, homography_matrix, dst.size());
         cv::imshow(selectWinName, frame);
+        cv::imshow("result", dst);
         if(cv::waitKey(10) > 0){
             break;
         }
     }
+    
     cv::destroyAllWindows();
 
     return 0;
